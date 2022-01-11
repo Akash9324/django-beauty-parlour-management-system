4e2f5e7bcd358d3055335bca154a97a63573e629
Akash Singh <akashrsingh9324@gmail.com>
2022-01-12 00:27:14 +0530

    first commit

---
+++ core/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('', views.index, name='index'), 
+   path('signup/', views.signup, name='signup'), 
+   path('login/', views.login, name='login'),
+   path('logout/', views.logout, name='logout'),
+   path('services/', views.services, name='services'), 
+   path('contact/', views.contact, name='contact'),
+   path('about/', views.about, name='about'),
+   path('bookappointment/', views.bookappointment, name='bookappointment'),
+   path('appointments/', views.appointments, name='appointments'),
+   path('admin_login/', views.admin_login, name='admin_login'),
+]
